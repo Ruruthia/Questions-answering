65c2f497012662fa6d6d3deed96e16c4a661bd03
Adrian Urbański <adrianu161999@gmail.com>
2022-12-11 11:03:46 +0100

    Add function for reading definitions

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import csv
+from collections import defaultdict
 
 
 def read_qa_tsv(questions_answers_path: str):
@@ -6,3 +7,12 @@ def read_qa_tsv(questions_answers_path: str):
         csv_file = csv.reader(in_file, delimiter='\t')
         qa_dict = {line[0]: line[1:] for line in csv_file}
     return qa_dict
+
+
+def read_definitions(definitions_path: str):
+    definitions_dict = defaultdict(list)
+    with open(definitions_path, 'r') as in_file:
+        for line in in_file:
+            word, definition = line.split('###')
+            definitions_dict[word.strip()].append(definition.strip())
+    return definitions_dict
