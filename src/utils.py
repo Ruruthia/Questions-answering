c160eb2708a4b1c579e8073cc4fbb43ad906d858
Maria Wyrzykowska <maria@pop-os.localdomain>
2022-12-11 17:50:42 +0100

    Update utils & requirements

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import csv
 from collections import defaultdict
 
+import editdistance
+
 
 def read_qa_tsv(questions_answers_path: str):
     with open(questions_answers_path, 'r') as in_file:
@@ -16,3 +18,20 @@ def read_definitions(definitions_path: str):
             word, definition = line.split('###')
             definitions_dict[word.strip()].append(definition.strip())
     return definitions_dict
+
+
+def scaled_editdist(ans: str, cor: str) -> float:
+    ans = ans.lower()
+    cor = cor.lower()
+
+    return editdistance.eval(ans, cor) / len(cor)
+
+
+def single_match(a: str, c: str) -> bool:
+    if c.isdecimal():
+        return a == c
+    return scaled_editdist(a, c) < 0.5
+
+
+def match(ans: str, cor: list[str]) -> bool:
+    return any(single_match(ans, c) for c in cor)
